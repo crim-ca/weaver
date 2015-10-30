eb3d259183400ab1cc0235ba1a7a9ce7122585b8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-10-30 16:14:54 +0100

    added no-check-cert option

--- twitcher/twitcherctl.py
+++ twitcher/twitcherctl.py
@@ -1,3 +1,6 @@
+import xmlrpclib
+import ssl
+
 import argcomplete
 import argparse
 
@@ -5,7 +8,25 @@ import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARN)
 logger = logging.getLogger(__name__)
 
+
+def _create_https_context(verify=True):
+    context = ssl._create_default_https_context()
+    if verify == False:
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+    return context
+
 class TwitcherCtl(object):
+
+    def __init__(self, url="https://localhost:38083/api/xmlrpc"):
+        self.url = url
+
+        
+    def create_server(self, verify=True):
+        context = _create_https_context(verify=verify)
+        server = xmlrpclib.ServerProxy(self.url, context=context)
+        return server
+        
 
     def create_parser(self):
         parser = argparse.ArgumentParser(
@@ -14,7 +35,10 @@ class TwitcherCtl(object):
             description='twitcherctl -- control twitcher proxy service from the cmd line.',
             )
         parser.add_argument("--debug",
-                            help="enable debug mode",
+                            help="enable debug mode.",
+                            action="store_true")
+        parser.add_argument("--no-check-certificate",
+                            help="don't validate the server's certificate.",
                             action="store_true")
 
         return parser
@@ -22,6 +46,8 @@ class TwitcherCtl(object):
     def run(self, args):
         if args.debug:
             logger.setLevel(logging.DEBUG)
+
+        server = self.create_server()
         return None
 
 def main():
