209caa590a7ec15a96af85b99b0318969fb45568
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-10-30 16:26:14 +0100

    added RpcInterface class

--- twitcher/api.py
+++ twitcher/api.py
@@ -1,5 +1,31 @@
+from pyramid.view import view_config, view_defaults
 from pyramid_rpc.xmlrpc import xmlrpc_method
 
-@xmlrpc_method(endpoint='api')
-def say_hello(request, name):
-    return 'hello, %s!' % name
+import logging
+logger = logging.getLogger(__name__)
+
+@view_defaults(permission='view')
+class RpcInterface(object):
+    def __init__(self, request):
+        self.request = request
+        self.session = self.request.session
+
+        
+    @xmlrpc_method(endpoint='api')
+    def addService(self, url, identifier):
+        return 'add service done'
+       
+
+    @xmlrpc_method(endpoint='api')
+    def removeService(self, identifier):
+        return 'remove service %s done' % identifier
+
+    
+    @xmlrpc_method(endpoint='api')
+    def listServices(self):
+        return 'list services done'
+
+    
+    @xmlrpc_method(endpoint='api')
+    def clearServices(self):
+        return 'clear services done'
