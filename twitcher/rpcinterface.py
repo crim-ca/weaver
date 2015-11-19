50b8cf5b4d43bdbd0b729bc5101a72a2a5fe761a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-11-19 18:23:00 +0100

    using rpcinterface class

--- twitcher/rpcinterface.py
+++ twitcher/rpcinterface.py
@@ -12,53 +12,58 @@ logger = logging.getLogger(__name__)
 # shortcut for xmlrpc_method
 api_xmlrpc = functools.partial(xmlrpc_method, endpoint="api")
 
+#@view_defaults(permission='admin')
+class RPCInterface(object):
+    def __init__(self, request):
+        self.request = request
 
-# token management
+    # token management
 
-@api_xmlrpc
-def generateToken(request):
-    access_token = tokens.generate_access_token(request)
-    return access_token.access_token
+    @api_xmlrpc
+    def generateToken(self):
+        access_token = tokens.generate_access_token(self.request)
+        return access_token.access_token
 
 
-# service registry
+    # service registry
 
-@api_xmlrpc()
-def addService(request, url):
-    service = registry.add_service(request, url=url)
-    return service['name']
+    @api_xmlrpc()
+    def addService(self, url):
+        service = registry.add_service(self.request, url=url)
+        return service['name']
 
-    
-@api_xmlrpc()
-def removeService(request, name):
-    try:
-        registry.remove_service(request, service_name=name)
-    except:
-        logger.exception('unregister failed')
-        return False
-    else:
-        return True
 
-    
-@api_xmlrpc()
-def listServices(request):
-    try:
-        services = registry.list_services(request)
-        return services
-    except:
-        logger.exception('register failed')
-        return []
+    @api_xmlrpc()
+    def removeService(self, name):
+        try:
+            registry.remove_service(self.request, service_name=name)
+        except:
+            logger.exception('unregister failed')
+            return False
+        else:
+            return True
 
-    
-@api_xmlrpc()
-def clearServices(request):
-    try:
-        registry.clear_service(request)
-    except:
-        logger.exception('clear failed')
-        return False
-    else:
-        return True
+
+
+    @api_xmlrpc()
+    def listServices(self):
+        try:
+            services = registry.list_services(self.request)
+            return services
+        except:
+            logger.exception('register failed')
+            return []
+
+
+    @api_xmlrpc()
+    def clearServices(self):
+        try:
+            registry.clear_service(self.request)
+        except:
+            logger.exception('clear failed')
+            return False
+        else:
+            return True
 
 def includeme(config):
     """ The callable makes it possible to include rpcinterface
