c51ed88b4757ea66a9ecd6d867476a0c38b7ff20
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-11-27 15:01:57 +0100

    using AccessTokenTestCase

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,35 +1,38 @@
 from nose.tools import ok_, assert_raises
+import unittest
 
 from datetime import timedelta
 
 from twitcher.utils import now
 from twitcher.tokens import AccessToken
 
-def test_access_token():
-    creation_time = now()
-    
-    token = AccessToken(token='abcdef', creation_time=creation_time)
-    ok_(token.not_before() == creation_time)
-    ok_(token.not_after() > creation_time)
-    ok_(token.is_valid() == True)
+class AccessTokenTestCase(unittest.TestCase):
 
-def test_bad_access_token():
-    with assert_raises(TypeError) as e:
-        AccessToken()
-    with assert_raises(TypeError) as e:
-        AccessToken(token='12345')
+    def test_access_token(self):
+        creation_time = now()
 
-def test_invalid_access_token():
-    creation_time = now() - timedelta(hours=2)
-    
-    token = AccessToken(token='abcdef', creation_time=creation_time)
-    ok_(token.not_before() == creation_time)
-    ok_(token.not_after() > creation_time)
-    ok_(token.is_valid() == False)
+        token = AccessToken(token='abcdef', creation_time=creation_time)
+        ok_(token.not_before() == creation_time)
+        ok_(token.not_after() > creation_time)
+        ok_(token.is_valid() == True)
+
+    def test_bad_access_token(self):
+        with assert_raises(TypeError) as e:
+            AccessToken()
+        with assert_raises(TypeError) as e:
+            AccessToken(token='12345')
+
+    def test_invalid_access_token(self):
+        creation_time = now() - timedelta(hours=2)
+
+        token = AccessToken(token='abcdef', creation_time=creation_time)
+        ok_(token.not_before() == creation_time)
+        ok_(token.not_after() > creation_time)
+        ok_(token.is_valid() == False)
 
 
-def test_access_token_with_user_environ():
-    creation_time = now()
-    token = AccessToken(token='12345', creation_time=creation_time,
-                        user_environ={'oauth_token': 'bfghk'})
-    ok_(token.user_environ == {'oauth_token': 'bfghk'})
+    def test_access_token_with_user_environ(self):
+        creation_time = now()
+        token = AccessToken(token='12345', creation_time=creation_time,
+                            user_environ={'oauth_token': 'bfghk'})
+        ok_(token.user_environ == {'oauth_token': 'bfghk'})
