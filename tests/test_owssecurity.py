50b273895409e07fb12ea4e7a3200fd5d29a8a31
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-11-30 13:55:54 +0100

    get_token tests added

--- tests/test_owssecurity.py
+++ tests/test_owssecurity.py
@@ -5,6 +5,7 @@ import mock
 from pyramid.testing import DummyRequest
 
 from twitcher.owssecurity import OWSSecurity
+from twitcher.owsexceptions import OWSAccessForbidden
 
 
 class OWSSecurityTestCase(unittest.TestCase):
@@ -15,5 +16,31 @@ class OWSSecurityTestCase(unittest.TestCase):
     def test_get_token_by_param(self):
         params = dict(request="Execute", service="WPS", access_token="abcdef")
         request = DummyRequest(params=params)
-        self.security.get_token(request)
+        token = self.security.get_token(request)
+        ok_(token == "abcdef")
+
+
+    def test_get_token_by_path(self):
+        params = dict(request="Execute", service="WPS")
+        request = DummyRequest(params=params, path="/ows/emu/12345")
+        token = self.security.get_token(request)
+        ok_(token == "12345")
+
+
+    def test_get_token_by_header(self):
+        params = dict(request="Execute", service="WPS")
+        headers = {'Access-Token': '54321'}
+        request = DummyRequest(params=params, headers=headers)
+        token = self.security.get_token(request)
+        ok_(token == "54321")
+
+
+    def test_get_token_forbidden(self):
+        params = dict(request="Execute", service="WPS")
+        request = DummyRequest(params=params)
+        with assert_raises(OWSAccessForbidden):
+            self.security.get_token(request)
+
+
+
 
