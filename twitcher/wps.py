4a0d0e6e054bc7fdeda1cde98319d2b99557101b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-12-01 14:40:42 +0100

    added wps_cfg and wps_environ_keys to twitcher.ini

--- twitcher/wps.py
+++ twitcher/wps.py
@@ -4,7 +4,7 @@ pywps wrapper
 import os
 
 from pyramid.httpexceptions import HTTPBadRequest
-from pyramid.settings import asbool
+from pyramid.settings import asbool, aslist
 
 import pywps
 from pywps.Exceptions import WPSException
@@ -13,8 +13,22 @@ from twitcher.owsexceptions import OWSNoApplicableCode
 import logging
 logger = logging.getLogger(__name__)
 
-wps_environ_keys = ['PYWPS_CFG', 'PYWPS_PROCESSES', 'PYWPS_TEMPLATES']
+DEFAULT_KEYS =  ['PYWPS_CFG', 'PYWPS_PROCESSES', 'PYWPS_TEMPLATES']
 
+def _wps_environ_keys(request):
+    settings = request.registry.settings
+    if 'twitcher.wps_environ_keys' in settings:
+        keys = aslist(settings['twitcher.wps_environ_keys'])
+    else:
+        keys = DEFAULT_KEYS
+    return keys
+
+
+def _wps_cfg(request):
+    settings = request.registry.settings
+    return settings.get('twitcher.wps_cfg')
+
+    
 def pywps_view(request):
     """
     * TODO: add xml response renderer
@@ -36,8 +50,11 @@ def pywps_view(request):
     if not inputQuery:
         return OWSNoApplicableCode("No query string found.")
 
+    if request.wps_cfg:
+        os.environ['PYWPS_CFG'] = request.wps_cfg
+
     # set the environ for wps from request environ
-    for key in wps_environ_keys:
+    for key in request.wps_environ_keys:
         if key in request.environ:
             os.environ[key] = request.environ[key]
 
@@ -57,10 +74,13 @@ def includeme(config):
 
     if asbool(settings.get('twitcher.wps', True)):
         logger.info('Add twitcher wps application')
+
         config.add_route('wps', '/ows/wps')
         config.add_route('wps_secured', '/ows/wps/{access_token}')
         config.add_view(pywps_view, route_name='wps', renderer='string')
         config.add_view(pywps_view, route_name='wps_secured', renderer='string')
+        config.add_request_method(_wps_environ_keys, 'wps_environ_keys', reify=True)
+        config.add_request_method(_wps_cfg, 'wps_cfg', reify=True)
 
 
 
