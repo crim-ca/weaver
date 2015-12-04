3e6002f52d07c4034a6d19dde8af72c14c3c994a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-12-04 17:36:59 +0100

    travis: enabled osx tests ... fixing mongodb

--- tests/functional/test_owsproxy_app.py
+++ tests/functional/test_owsproxy_app.py
@@ -8,7 +8,7 @@ from twitcher.registry import service_registry_factory
 class OWSProxyAppTest(unittest.TestCase):
 
     def setUp(self):
-        settings = {'mongodb.host':'localhost', 'mongodb.port':'27027', 'mongodb.db_name': 'twitcher_test'}
+        settings = {'mongodb.host':'127.0.0.1', 'mongodb.port':'27027', 'mongodb.db_name': 'twitcher_test'}
         config = testing.setUp(settings=settings)
         config.include('twitcher.owsproxy')
         config.include('twitcher.tweens')
@@ -21,11 +21,11 @@ class OWSProxyAppTest(unittest.TestCase):
     def _setup_db(self, config):
         registry = service_registry_factory(config.registry)
         registry.clear_services()
-        registry.register_service(url="http://not.on.earth/wps", name="earth")
+        registry.register_service(url="https://localhost:38083/ows/wps", name="twitcher")
 
     @attr('online')
     def test_execute_not_allowed(self):
-        resp = self.app.get('/ows/proxy/earth?service=wps&request=execute&version=1.0.0&identifier=dummyprocess')
+        resp = self.app.get('/ows/proxy/twitcher?service=wps&request=execute&version=1.0.0&identifier=dummyprocess')
         assert resp.status_code == 200
         assert resp.content_type == 'text/xml'
         print resp.body
