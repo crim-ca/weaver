435640d8a7dd85f252d7190b8088916097e562e9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-04-18 10:24:04 +0200

    using versions.cfg

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -311,10 +311,15 @@ texinfo_documents = [
 intersphinx_mapping = {'python': ('http://docs.python.org/', None),
                        'phoenix': ('http://pyramid-phoenix.readthedocs.org/en/latest/', None),
                        'malleefowl': ('http://malleefowl.readthedocs.org/en/latest/', None),
+                       'twitcher': ('http://twitcher.readthedocs.org/en/latest/', None),
                        'flyingpigeon': ('http://flyingpigeon.readthedocs.org/en/latest/', None),
                        'hummingbird': ('http://birdhouse-hummingbird.readthedocs.org/en/latest/', None),
                        'emu': ('http://emu.readthedocs.org/en/latest/', None),
                        'birdy': ('http://birdy.readthedocs.org/en/latest/', None),
                        'bootstrap': ('http://birdhousebuilderbootstrap.readthedocs.org/en/latest/', None),
                         }
+
+# linkcheck options
+# http://www.sphinx-doc.org/en/stable/config.html?highlight=linkchecker#options-for-the-linkcheck-builder
+linkcheck_ignore = [r'http://localhost:\d+/']
 
