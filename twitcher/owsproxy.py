55bc850d8f3778f223266cb1b60ca679be938485
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-09 14:58:46 +0200

    pep8, working on extra_path

--- twitcher/owsproxy.py
+++ twitcher/owsproxy.py
@@ -5,9 +5,8 @@ The owsproxy is based on `papyrus_ogcproxy <https://github.com/elemoine/papyrus_
 import urllib
 import requests
 
-from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPBadRequest, HTTPBadGateway, HTTPNotAcceptable, HTTPForbidden
-from twitcher.owsexceptions import OWSNoApplicableCode, OWSAccessForbidden
+from pyramid.httpexceptions import HTTPBadRequest, HTTPBadGateway, HTTPNotAcceptable
+from twitcher.owsexceptions import OWSAccessForbidden
 from pyramid.response import Response
 from pyramid.settings import asbool
 
@@ -23,10 +22,10 @@ allowed_content_types = (
     "text/xml;charset=ISO-8859-1"
     "application/vnd.ogc.se_xml",            # OGC Service Exception
     "application/vnd.ogc.se+xml",            # OGC Service Exception
-    #"application/vnd.ogc.success+xml",      # OGC Success (SLD Put)
+    # "application/vnd.ogc.success+xml",      # OGC Success (SLD Put)
     "application/vnd.ogc.wms_xml",           # WMS Capabilities
-    #"application/vnd.ogc.gml",              # GML
-    #"application/vnd.ogc.sld+xml",          # SLD
+    # "application/vnd.ogc.gml",              # GML
+    # "application/vnd.ogc.sld+xml",          # SLD
     "application/vnd.google-earth.kml+xml",  # KML
     "application/vnd.google-earth.kmz",
     "image/png",                             # PNG
@@ -38,26 +37,29 @@ allowed_content_types = (
     )
 
           
-def _send_request(request, service):
+def _send_request(request, service, extra_path=None):
+    
     # TODO: fix way to build url
-    logger.debug('params = %s', request.params)
-    url = service['url'] + '?' + urllib.urlencode(request.params)
-
+    url = service['url']
+    if extra_path:
+        url += '/' + extra_path
+    url += '?' + urllib.urlencode(request.params)
+    # logger.debug('url = %s', url)
+    
     # forward request to target (without Host Header)
     h = dict(request.headers)
     h.pop("Host", h)
-    resp = None
     try:
         resp = requests.request(method=request.method.upper(), url=url, data=request.body, headers=h)
     except Exception, e:
         return HTTPBadGateway("Request failed: %s" % (e.message))
 
-    if resp.ok == False:
+    if resp.ok is False:
         return HTTPBadGateway("Response is not ok: %s" % (resp.reason))
 
     # check for allowed content types
     ct = None
-    #logger.debug("headers=", resp.headers)
+    # logger.debug("headers=", resp.headers)
     if "Content-Type" in resp.headers:
         ct = resp.headers["Content-Type"]
         if not ct.split(";")[0] in allowed_content_types:
@@ -65,15 +67,16 @@ def _send_request(request, service):
             logger.error(msg)
             return OWSAccessForbidden(msg)
     else:
-        #return HTTPNotAcceptable("Could not get content type from response.")
+        # return HTTPNotAcceptable("Could not get content type from response.")
         logger.warn("Could not get content type from response")
 
-    content = None
     try:
         if ct in ['text/xml', 'application/xml', 'text/xml;charset=ISO-8859-1']:
                 # replace urls in xml content
                 content = resp.content.decode('utf-8', 'ignore')
                 content = content.replace(service['url'], proxy_url(request, service['name']))
+                # TODO: remove default namespace in ncwms2 getcaps
+                # content = content.replace('xmlns="http://www.opengis.net/wms"', '')
         else:
             # raw content
             content = resp.content
@@ -85,21 +88,21 @@ def _send_request(request, service):
         headers["Content-Type"] = ct
     return Response(content, status=resp.status_code, headers=headers)
 
+
 def owsproxy_view(request):
     """
     TODO: use ows exceptions
     """
-    service_name = request.matchdict.get('service_name')
-    if service_name is None:
-        return HTTPBadRequest('Parameter service_name is required.')
-
     try:
+        service_name = request.matchdict.get('service_name')
+        extra_path = request.matchdict.get('extra_path')
         registry = service_registry_factory(request.registry)
         service = registry.get_service(service_name)
     except Exception as err:
         return HTTPBadRequest("Could not find service: %s." % (err.message))
     else:
-        return _send_request(request, service)
+        return _send_request(request, service, extra_path)
+
 
 def includeme(config):
     settings = config.registry.settings
@@ -109,8 +112,11 @@ def includeme(config):
         
         # include mongodb
         config.include('twitcher.db')
-    
+
         config.add_route('owsproxy', '/ows/proxy/{service_name}')
+        # TODO: maybe configure extra path
+        # config.add_route('owsproxy_extra', '/ows/proxy/{service_name}/{extra_path:.*}')
         config.add_route('owsproxy_secured', '/ows/proxy/{service_name}/{access_token}')
         config.add_view(owsproxy_view, route_name='owsproxy')
+        # config.add_view(owsproxy_view, route_name='owsproxy_extra')
         config.add_view(owsproxy_view, route_name='owsproxy_secured')
