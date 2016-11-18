cde20587c111b2543e390046af2f0f734450579c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-18 11:19:24 +0100

    fixed tests for c4i flag

--- twitcher/tests/test_registry.py
+++ twitcher/tests/test_registry.py
@@ -8,13 +8,15 @@ from twitcher.registry import parse_service_name
 
 class ServiceRegistryTestCase(unittest.TestCase):
     def setUp(self):
-        self.service = dict(name="loving_flamingo", url="http://somewhere.over.the/ocean", type="wps", public=False)
-        self.service_public = dict(name="open_pingu", url="http://somewhere.in.the/deep_ocean", type="wps", public=True)
+        self.service = dict(name="loving_flamingo", url="http://somewhere.over.the/ocean", type="wps",
+                            public=False, c4i=False)
+        self.service_public = dict(name="open_pingu", url="http://somewhere.in.the/deep_ocean", type="wps",
+                                   public=True, c4i=False)
 
     def test_get_service(self):
         collection_mock = mock.Mock(spec=["find_one"])
         collection_mock.find_one.return_value = self.service
-        
+
         registry = ServiceRegistry(collection=collection_mock)
         service = registry.get_service(name=self.service['name'])
 
@@ -24,7 +26,7 @@ class ServiceRegistryTestCase(unittest.TestCase):
     def test_register_service_default(self):
         collection_mock = mock.Mock(spec=["insert_one", "find_one", "count"])
         collection_mock.count.return_value = 0
-        
+
         store = ServiceRegistry(collection=collection_mock)
         store.register_service(url=self.service['url'], name=self.service['name'])
 
@@ -33,17 +35,17 @@ class ServiceRegistryTestCase(unittest.TestCase):
     def test_register_service_with_special_name(self):
         collection_mock = mock.Mock(spec=["insert_one", "find_one", "count"])
         collection_mock.count.return_value = 0
-        
+
         store = ServiceRegistry(collection=collection_mock)
         store.register_service(url="http://wonderload", name="A special Name")
 
         collection_mock.insert_one.assert_called_with({
-            'url': 'http://wonderload', 'type': 'wps', 'name': 'a_special_name', 'public': False})
+            'url': 'http://wonderload', 'type': 'wps', 'name': 'a_special_name', 'public': False, 'c4i': False})
 
     def test_register_service_public(self):
         collection_mock = mock.Mock(spec=["insert_one", "find_one", "count"])
         collection_mock.count.return_value = 0
-        
+
         store = ServiceRegistry(collection=collection_mock)
         store.register_service(url=self.service_public['url'], name=self.service_public['name'], public=True)
 
@@ -57,4 +59,3 @@ class ServiceRegistryTestCase(unittest.TestCase):
             assert 'emu' == parse_service_name("/ows/proxy/")
         with pytest.raises(ValueError) as e_info:
             assert 'emu' == parse_service_name("/ows/nowhere/emu")
-
