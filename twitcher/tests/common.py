302628c15d06ba87494ac2393acfd1465f09abfe
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-08 14:13:19 +0100

    added wms resource

--- twitcher/tests/common.py
+++ twitcher/tests/common.py
@@ -3,3 +3,4 @@ import os
 RESOURCES_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources'))
 
 WPS_CAPS_EMU_XML = os.path.join(RESOURCES_PATH, 'wps_caps_emu.xml')
+WMS_CAPS_NCWMS2_XML = os.path.join(RESOURCES_PATH, 'wms_caps_ncwms2.xml')
