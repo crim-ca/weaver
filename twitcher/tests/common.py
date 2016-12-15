2025046675326050793f77cd169076c88299b265
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-15 17:17:56 +0100

    added wms_130 and renamed wms_111

--- twitcher/tests/common.py
+++ twitcher/tests/common.py
@@ -3,4 +3,5 @@ import os
 RESOURCES_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources'))
 
 WPS_CAPS_EMU_XML = os.path.join(RESOURCES_PATH, 'wps_caps_emu.xml')
-WMS_CAPS_NCWMS2_XML = os.path.join(RESOURCES_PATH, 'wms_caps_ncwms2.xml')
+WMS_CAPS_NCWMS2_111_XML = os.path.join(RESOURCES_PATH, 'wms_caps_ncwms2_111.xml')
+WMS_CAPS_NCWMS2_130_XML = os.path.join(RESOURCES_PATH, 'wms_caps_ncwms2_130.xml')
