03d62134afad47fb8d7bb92534738380f91cca7c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-16 16:14:12 +0100

    using oauth2session

--- twitcher/esgf.py
+++ twitcher/esgf.py
@@ -17,6 +17,7 @@ import os
 from OpenSSL import crypto
 import base64
 import requests
+from requests_oauthlib import OAuth2Session
 
 import logging
 logger = logging.getLogger(__name__)
@@ -51,44 +52,57 @@ class ESGFAccessManager(object):
         self.esgf_certs_dir = os.path.join(self.esgf_dir, ESGF_CERTS_DIR)
         self.dap_config = dap_config or DAP_CONFIG
 
+        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
     def logon(self, access_token):
         cert = self._get_certificate(access_token)
         self._write_certificate(cert)
         self._write_dap_config()
 
-    def _get_certificate(self, access_token):
+    def _get_certificate(self, client_id, access_token):
+        """
+        Generates a new private key and certificate request, submits the request to be
+        signed by the SLCS CA and returns the certificate.
+        """
         # Generate a new key pair
         key_pair = crypto.PKey()
         key_pair.generate_key(crypto.TYPE_RSA, 2048)
         private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, key_pair).decode("utf-8")
 
         # Generate a certificate request using that key-pair
-        cert_req = crypto.X509Req()
+        cert_request = crypto.X509Req()
 
         # Create public key object
-        cert_req.set_pubkey(key_pair)
+        cert_request.set_pubkey(key_pair)
 
         # Add the public key to the request
-        cert_req.sign(key_pair, 'md5')
-
-        der_cert_req = crypto.dump_certificate_request(crypto.FILETYPE_ASN1,
-                                                       cert_req)
+        cert_request.sign(key_pair, 'md5')
+        der_cert_req = crypto.dump_certificate_request(crypto.FILETYPE_ASN1, cert_request)
 
         encoded_cert_req = base64.b64encode(der_cert_req)
 
-        headers = {}
-        headers['Authorization'] = 'Bearer %s' % access_token
+        # Build the OAuth session object
+        token = {'access_token': access_token, 'token_type': 'Bearer'}
+        slcs = OAuth2Session(client_id, token=token)
+        #headers = {}
+        #headers['Authorization'] = 'Bearer %s' % access_token
         #post_data = urllib.urlencode({'certificate_request': encoded_cert_req})
-        post_data = {'certificate_request': encoded_cert_req}
 
-        logger.debug(headers)
-        logger.debug(post_data)
+        response = slcs.post(
+            self.url,
+            data={'certificate_request': encoded_cert_req},
+            verify=False
+        )
 
-        r = requests.post(self.url,
-                          headers=headers,
-                          data=post_data,
-                          verify=False)
-        content = "{} {}".format(r.text, private_key)
+        # response = requests.post(self.url,
+        #                          headers=headers,
+        #                          data=post_data,
+        #                          verify=False)
+        if response.status_code == 200:
+            content = "{} {}".format(response.text, private_key)
+        else:
+            msg = "Could not get certificate: {} {}".format(response.status_code, response.reason)
+            raise Exception(msg)
         return content
 
     def _write_certificate(self, certificate):
