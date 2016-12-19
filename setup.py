fdc6d783262539a2e826411f61026c9c62fec465
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-19 11:28:40 +0100

    using version twticher.__version__

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup, find_packages
 
+version = __import__('twitcher').__version__
+
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
@@ -8,16 +10,16 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 reqs = [line.strip() for line in open('requirements/deploy.txt')]
 
 setup(name='pyramid_twitcher',
-      version='0.2.4',
+      version=version,
       description='Security Proxy for OGC Services like WPS.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Development Status :: 4 - Beta",
-        ],
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "Development Status :: 4 - Beta",
+      ],
       author='Birdhouse Developers',
       author_email='wps-dev@lists.dkrz.de',
       url='https://github.com/bird-house/twitcher.git',
