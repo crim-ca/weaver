a47578a007cf95e96be2e56e9b0616f10c10e497
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-04 18:29:07 +0100

    added initial python 3 compat module

---
+++ twitcher/_compat.py
@@ -0,0 +1,28 @@
+"""
+This python 2.x/python 3.x compatibility modules is based on the pywps 4.x code.
+"""
+
+import logging
+import sys
+
+
+LOGGER = logging.getLogger('twitcher')
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    LOGGER.debug('Python 2.x')
+    text_type = unicode  # noqa
+    from StringIO import StringIO
+    from flufl.enum import Enum
+    from urlparse import urlparse
+    from urlparse import urljoin
+    from urllib2 import urlopen
+
+else:
+    LOGGER.debug('Python 3.x')
+    text_type = str
+    from io import StringIO
+    from enum import Enum
+    from urllib.parse import urlparse
+    from urllib.parse import urljoin
+    from urllib.request import urlopen
