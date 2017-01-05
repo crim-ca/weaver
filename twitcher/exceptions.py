cb925d00d1fae443c0d9aa52d00333a8f9e27e4a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-05 14:21:31 +0100

    update comments

--- twitcher/exceptions.py
+++ twitcher/exceptions.py
@@ -1,4 +1,13 @@
+"""
+Errors raised during the Twitcher flow.
+"""
+
+
 class AccessTokenNotFound(Exception):
+    """
+    Error indicating that an access token could not be read from the
+    storage backend by an instance of :class:`twitcher.store.AccessTokenStore`.
+    """
     pass
 
 
