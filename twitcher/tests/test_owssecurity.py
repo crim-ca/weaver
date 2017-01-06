91ca51a60efa12ff9a84cd364e4c0bf190e392d3
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-06 18:10:01 +0100

    fixed owssecurity tests

--- twitcher/tests/test_owssecurity.py
+++ twitcher/tests/test_owssecurity.py
@@ -5,6 +5,7 @@ import mock
 from pyramid.testing import DummyRequest
 
 from twitcher.datatype import AccessToken
+from twitcher.datatype import Service
 from twitcher.utils import expires_at
 from twitcher.owssecurity import OWSSecurity
 from twitcher.owsexceptions import OWSAccessForbidden
@@ -14,13 +15,14 @@ class OWSSecurityTestCase(unittest.TestCase):
     def setUp(self):
         self.access_token = AccessToken(token="cdefg", expires_at=expires_at(hours=1))
 
-        self.store_mock = mock.Mock(spec=["fetch_by_token"])
-        self.store_mock.fetch_by_token.return_value = self.access_token
+        self.tokenstore_mock = mock.Mock(spec=["fetch_by_token"])
+        self.tokenstore_mock.fetch_by_token.return_value = self.access_token
 
-        self.registry_mock = mock.Mock(spec=["is_public"])
-        self.registry_mock.is_public.return_value = False
+        self.servicestore_mock = mock.Mock(spec=["fetch_by_name"])
+        self.servicestore_mock.fetch_by_name.return_value = Service(
+            url='http://nowhere/wps', name='test_wps', public=False)
 
-        self.security = OWSSecurity(tokenstore=self.store_mock, service_registry=self.registry_mock)
+        self.security = OWSSecurity(tokenstore=self.tokenstore_mock, servicestore=self.servicestore_mock)
 
     def test_get_token_by_param(self):
         params = dict(request="Execute", service="WPS", access_token="abcdef")
@@ -47,9 +49,9 @@ class OWSSecurityTestCase(unittest.TestCase):
         self.security.check_request(request)
 
     def test_check_request_invalid(self):
-        store_mock = mock.Mock(spec=["fetch_by_token"])
-        store_mock.fetch_by_token.return_value = None
-        security = OWSSecurity(tokenstore=store_mock, service_registry=self.registry_mock)
+        tokenstore_mock = mock.Mock(spec=["fetch_by_token"])
+        tokenstore_mock.fetch_by_token.return_value = None
+        security = OWSSecurity(tokenstore=tokenstore_mock, servicestore=self.servicestore_mock)
 
         params = dict(request="Execute", service="WPS", version="1.0.0", access_token="xyz")
         request = DummyRequest(params=params, path='/ows/proxy/emu')
@@ -59,7 +61,7 @@ class OWSSecurityTestCase(unittest.TestCase):
     def test_check_request_allowed_caps(self):
         store_mock = mock.Mock(spec=["fetch_by_token"])
         store_mock.fetch_by_token.return_value = None
-        security = OWSSecurity(tokenstore=store_mock, service_registry=self.registry_mock)
+        security = OWSSecurity(tokenstore=store_mock, servicestore=self.servicestore_mock)
 
         params = dict(request="GetCapabilities", service="WPS", version="1.0.0")
         request = DummyRequest(params=params, path='/ows/proxy/emu')
@@ -68,16 +70,17 @@ class OWSSecurityTestCase(unittest.TestCase):
     def test_check_request_allowed_describeprocess(self):
         store_mock = mock.Mock(spec=["fetch_by_token"])
         store_mock.fetch_by_token.return_value = None
-        security = OWSSecurity(tokenstore=store_mock, service_registry=self.registry_mock)
+        security = OWSSecurity(tokenstore=store_mock, servicestore=self.servicestore_mock)
 
         params = dict(request="DescribeProcess", service="WPS", version="1.0.0")
         request = DummyRequest(params=params, path='/ows/proxy/emu')
         security.check_request(request)
 
     def test_check_request_public_access(self):
-        registry_mock = mock.Mock(spec=["is_public"])
-        registry_mock.is_public.return_value = True
-        security = OWSSecurity(tokenstore=self.store_mock, service_registry=registry_mock)
+        servicestore_mock = mock.Mock(spec=["fetch_by_name"])
+        servicestore_mock.fetch_by_name.return_value = Service(
+            url='http://nowhere/wps', name='test_wps', public=True)
+        security = OWSSecurity(tokenstore=self.tokenstore_mock, servicestore=servicestore_mock)
 
         params = dict(request="Execute", service="WPS", version="1.0.0")
         request = DummyRequest(params=params, path='/ows/proxy/emu')
