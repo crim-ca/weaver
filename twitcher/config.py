b7f100920bf841635590170633333f0e24dcbce3
David Byrns <david.byrns@crim.ca>
2018-06-21 16:21:32 -0400

    Allow passing extra options into pyramid config from buildout without need to be known by the twitcher ini template

--- twitcher/config.py
+++ twitcher/config.py
@@ -25,6 +25,33 @@ def _prefix(request):
     return prefix
 
 
+def parse_extra_options(option_str):
+    """
+    Parses the extra options parameter.
+
+    The option_str is a string with ``opt=value`` pairs.
+
+    Example::
+
+        tempdir=/path/to/tempdir
+        archive_root=/path/to/archive
+
+    :param option_str: A string parameter with the extra options.
+    :return: A dict with the parsed extra options.
+    """
+    if option_str:
+        try:
+            extra_options = option_str.split(',')
+            extra_options = dict([('=' in opt) and opt.split('=', 1) for opt in extra_options])
+        except Exception:
+            msg = "Can not parse extra-options: {}".format(option_str)
+            logging.exception(msg)
+            raise zc.buildout.UserError(msg)
+    else:
+        extra_options = {}
+    return extra_options
+
+
 def includeme(config):
     # settings = config.registry.settings
 
@@ -32,3 +59,6 @@ def includeme(config):
 
     config.add_request_method(_workdir, 'workdir', reify=True)
     config.add_request_method(_prefix, 'prefix', reify=True)
+
+    settings = config.registry.settings
+    config.add_settings(parse_extra_options(settings.get('twitcher.extra_options', '')))
