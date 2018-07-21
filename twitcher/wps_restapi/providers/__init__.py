6d97ac3230f4c17c0235214078b58c0d82154fd6
David Byrns <david.byrns@crim.ca>
2018-07-20 21:33:36 -0400

    Cleanup providers and exceptions handling

--- twitcher/wps_restapi/providers/__init__.py
+++ twitcher/wps_restapi/providers/__init__.py
@@ -1,13 +1,15 @@
-import logging
-logger = logging.getLogger(__name__)
+from twitcher.wps_restapi.providers.providers import get_providers, add_provider, get_provider, remove_provider
 
-from twitcher.wps_restapi.providers.providers import get_providers, add_provider, get_capabilities, remove_provider
+import logging
+logger = logging.getLogger('TWITCHER')
 
 
 def includeme(config):
+    logger.debug('Loading providers config.')
+
     config.add_route('providers', '/providers')
     config.add_route('provider', '/providers/{provider_id}')
     config.add_view(get_providers, route_name='providers', request_method='GET', renderer='json')
     config.add_view(add_provider, route_name='providers', request_method='POST', renderer='json')
-    config.add_view(get_capabilities, route_name='provider', request_method='GET', renderer='json')
+    config.add_view(get_provider, route_name='provider', request_method='GET', renderer='json')
     config.add_view(remove_provider, route_name='provider', request_method='DELETE', renderer='json')
--- twitcher/wps_restapi/providers/providers.py
+++ twitcher/wps_restapi/providers/providers.py
@@ -1,9 +1,14 @@
-from pyramid.view import view_config
-from twitcher.adapter import servicestore_factory
-from owslib.wps import WebProcessingService
-from twitcher.datatype import Service
-from twitcher.wps_restapi.utils import restapi_base_url, get_cookie_headers
+from pyramid.response import Response
 import pyramid.httpexceptions as exc
+from owslib.wps import WebProcessingService
+
+from twitcher.adapter import servicestore_factory
+from twitcher.datatype import Service
+from twitcher.exceptions import ServiceNotFound
+from twitcher.wps_restapi.utils import restapi_base_url, get_cookie_headers
+
+import logging
+logger = logging.getLogger('TWITCHER')
 
 
 def get_providers(request):
@@ -25,9 +30,44 @@ def get_providers(request):
                     provider_id=service.name),
                 public=service.public))
         except Exception as e:
+            logger.warn('Exception occurs while fetching wps {0} : {1!r}'.format(service.url, e))
             pass
 
     return providers
+
+
+def get_capabilities(service, request):
+    """
+    GetCapabilities of a wps provider
+    """
+    wps = WebProcessingService(url=service.url, headers=get_cookie_headers(request.headers))
+
+    return dict(
+        id=service.name,
+        title=wps.identification.title,
+        abstract=wps.identification.abstract,
+        url='{base_url}/providers/{provider_id}'.format(
+            base_url=restapi_base_url(request),
+            provider_id=service.name),
+        processes='{base_url}/providers/{provider_id}/processes'.format(
+            base_url=restapi_base_url(request),
+            provider_id=service.name),
+        type='WPS',
+        contact=wps.provider.contact.name)
+
+
+def get_service(request):
+    """
+    Get the request service using provider_id from the service store
+    """
+    store = servicestore_factory(request.registry)
+    provider_id = request.matchdict.get('provider_id')
+    try:
+        service = store.fetch_by_name(provider_id, request=request)
+    except ServiceNotFound:
+        logger.warn('Provider {0} cannot be found'.format(provider_id))
+        raise exc.HTTPNotFound('Provider {0} cannot be found'.format(provider_id))
+    return service, store
 
 
 def add_provider(request):
@@ -36,8 +76,12 @@ def add_provider(request):
     """
     store = servicestore_factory(request.registry)
 
-    # TODO Validate that params have at least a url and a name
-    new_service = Service(url=request.json['url'], name=request.json['id'])
+    try:
+        new_service = Service(url=request.json['url'], name=request.json['id'])
+    except KeyError as e:
+        logger.warn('Missing json parameter {0}'.format(e))
+        raise exc.HTTPBadRequest(detail='Missing json parameter {0}'.format(e))
+
     if 'public' in request.json:
         new_service['public'] = request.json['public']
     if 'auth' in request.json:
@@ -46,50 +90,30 @@ def add_provider(request):
     try:
         store.save_service(new_service, request=request)
     except NotImplementedError:
+        logger.warn('Add provider not supported')
         raise exc.HTTPNotImplemented(detail='Add provider not supported')
 
-    return {}
+    return get_capabilities(new_service, request)
 
 
 def remove_provider(request):
     """
     Remove a provider
     """
-    store = servicestore_factory(request.registry)
-
-    # TODO Validate param somehow
-    provider_id = request.matchdict.get('provider_id')
+    service, store = get_service(request)
 
     try:
-        store.delete_service(provider_id, request=request)
+        store.delete_service(service.name, request=request)
     except NotImplementedError:
-        raise exc.HTTPNotImplemented(detail='Add provider not supported')
+        logger.warn('Delete provider not supported')
+        raise exc.HTTPNotImplemented(detail='Delete provider not supported')
 
-    return {}
+    return Response(status=204)
 
 
-def get_capabilities(request):
+def get_provider(request):
     """
     GetCapabilities of a wps provider
     """
-
-    store = servicestore_factory(request.registry)
-
-    # TODO Validate param somehow
-    provider_id = request.matchdict.get('provider_id')
-
-    service = store.fetch_by_name(provider_id, request=request)
-    wps = WebProcessingService(url=service.url, headers=get_cookie_headers(request.headers))
-
-    return dict(
-        id=provider_id,
-        title=wps.identification.title,
-        abstract=wps.identification.abstract,
-        url='{base_url}/providers/{provider_id}'.format(
-            base_url=restapi_base_url(request),
-            provider_id=provider_id),
-        processes='{base_url}/providers/{provider_id}/processes'.format(
-            base_url=restapi_base_url(request),
-            provider_id=provider_id),
-        type='WPS',
-        contact=wps.provider.contact.name)
+    service, store = get_service(request)
+    return get_capabilities(service, request)
