87a070ebcc4eadf97cf5a5d9c1f41471e88cd80d
David Caron <dcaron05@gmail.com>
2018-10-04 14:36:48 -0400

    refactor

--- twitcher/tests/test_wps_package.py
+++ twitcher/tests/test_wps_package.py
@@ -26,6 +26,23 @@ def assert_json_equals(json1, json2):
         assert line1 == line2
 
 
+def get_test_file(filename):
+    return os.path.join(os.path.dirname(__file__), 'json_examples', filename)
+
+
+def load_json(filename):
+    return json.load(open(get_test_file(filename)))
+
+
+def make_request(**kw):
+    request = DummyRequest(**kw)
+    if request.registry.settings is None:
+        request.registry.settings = {}
+    request.registry.settings['twitcher.url'] = "localhost"
+    request.registry.settings['twitcher.db_factory'] = DB_MEMORY
+    return request
+
+
 class WpsHandleEOITestCase(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
@@ -33,40 +50,28 @@ class WpsHandleEOITestCase(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
-    def get_test_file(self, filename):
-        return os.path.join(os.path.dirname(__file__), 'json_examples', filename)
-
-    def load_json(self, filename):
-        return json.load(open(self.get_test_file(filename)))
-
-    def make_request(self, **kw):
-        request = DummyRequest(**kw)
-        request.registry.settings['twitcher.url'] = "localhost"
-        request.registry.settings['twitcher.db_factory'] = DB_MEMORY
-        return request
-
     def test_handle_EOI_unique_aoi_unique_toi(self):
-        inputs = self.load_json("eoimage_inputs_example.json")
-        expected = self.load_json("eoimage_unique_aoi_unique_toi.json")
+        inputs = load_json("eoimage_inputs_example.json")
+        expected = load_json("eoimage_unique_aoi_unique_toi.json")
         output = wps_package.EOImageHandler(inputs).to_opensearch(unique_aoi=True, unique_toi=True)
         assert_json_equals(output, expected)
 
     def test_handle_EOI_unique_aoi_non_unique_toi(self):
-        inputs = self.load_json("eoimage_inputs_example.json")
-        expected = self.load_json("eoimage_unique_aoi_non_unique_toi.json")
+        inputs = load_json("eoimage_inputs_example.json")
+        expected = load_json("eoimage_unique_aoi_non_unique_toi.json")
         output = wps_package.EOImageHandler(inputs).to_opensearch(unique_aoi=True, unique_toi=False)
         assert_json_equals(expected, output)
 
     def test_handle_EOI_non_unique_aoi_unique_toi(self):
-        inputs = self.load_json("eoimage_inputs_example.json")
-        expected = self.load_json("eoimage_non_unique_aoi_unique_toi.json")
+        inputs = load_json("eoimage_inputs_example.json")
+        expected = load_json("eoimage_non_unique_aoi_unique_toi.json")
         output = wps_package.EOImageHandler(inputs).to_opensearch(unique_aoi=False, unique_toi=True)
         assert_json_equals(expected, output)
 
     def test_handle_EOI_multisensor_ndvi(self):
-        deploy = self.load_json("DeployProcess_Workflow_MultiSensor_NDVI_Stack_Generator_.json")
+        deploy = load_json("DeployProcess_Workflow_MultiSensor_NDVI_Stack_Generator_.json")
         inputs = deploy["processOffering"]["process"]["inputs"]
-        describe = self.load_json("DescribeProcessResponse_Multisensor_ndivi_stack_generator.json")
+        describe = load_json("DescribeProcessResponse_Multisensor_ndivi_stack_generator.json")
         expected = describe["processOffering"]["process"]["inputs"]
         output = wps_package.EOImageHandler(inputs).to_opensearch(unique_aoi=True, unique_toi=True)
         assert_json_equals(expected, output)
