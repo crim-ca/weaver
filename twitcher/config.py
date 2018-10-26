aee207dca74186f50bc5b2264891f1dc9ae44752
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-10-26 15:08:59 -0400

    backport twitcher config

--- twitcher/config.py
+++ twitcher/config.py
@@ -1,11 +1,29 @@
 import os
 import tempfile
-
+from pyramid.exceptions import ConfigurationError
 from pyramid.settings import asbool
 
 
 import logging
 LOGGER = logging.getLogger("TWITCHER")
+
+
+TWITCHER_CONFIGURATION_DEFAULT = 'DEFAULT'
+TWITCHER_CONFIGURATIONS = frozenset([
+    TWITCHER_CONFIGURATION_DEFAULT,
+    # TODO: add other configurations here
+])
+
+
+def get_twitcher_configuration(settings):
+    twitcher_config = settings.get('twitcher.configuration')
+    if not twitcher_config:
+        LOGGER.warn("Setting 'twitcher.configuration' not specified, using '{}'".format(TWITCHER_CONFIGURATION_DEFAULT))
+        twitcher_config = TWITCHER_CONFIGURATION_DEFAULT
+    twitcher_config_up = twitcher_config.upper()
+    if twitcher_config_up not in TWITCHER_CONFIGURATIONS:
+        raise ConfigurationError("Unknown setting 'twitcher.configuration' specified: '{}'".format(twitcher_config))
+    return twitcher_config_up
 
 
 def _workdir(request):
