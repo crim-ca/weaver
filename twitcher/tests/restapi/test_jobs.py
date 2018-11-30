919eb29cd14715e19793064f89bcbe56ddf304e1
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-11-30 13:24:53 -0500

    [WIP] testing job 'access'

--- twitcher/tests/restapi/test_jobs.py
+++ twitcher/tests/restapi/test_jobs.py
@@ -5,10 +5,25 @@ import webtest
 import unittest
 import pyramid.testing
 import six
+from typing import AnyStr, Tuple, List
 from twitcher.tests.utils import setup_config_with_mongodb, setup_mongodb_processstore, setup_mongodb_jobstore
 from twitcher.status import job_status_values, STATUS_SUCCEEDED, STATUS_FAILED
 from twitcher.processes.wps_testing import WpsTestProcess
 from twitcher.visibility import VISIBILITY_PUBLIC, VISIBILITY_PRIVATE
+
+
+class UserRequest(pyramid.testing.DummyRequest):
+    def __init__(self, user_id=None, is_admin=False, **kw):
+        super(UserRequest, self).__init__(**kw)
+        self.user_id = user_id
+        self.is_admin = is_admin
+
+    @property
+    def authenticated_userid(self):
+        return self.user_id
+
+    def has_permission(self, permission, context=None):
+        return self.is_admin if permission == 'admin' else False
 
 
 class WpsRestApiJobsTest(unittest.TestCase):
@@ -32,6 +47,10 @@ class WpsRestApiJobsTest(unittest.TestCase):
         self.job_store = setup_mongodb_jobstore(self.config)
         self.process_store = setup_mongodb_processstore(self.config)
 
+        self.user_admin_id = 100
+        self.user_editor1_id = 1
+        self.user_editor2_id = 2
+
         self.test_job1_process_id = 'process-1'
         self.process_public = WpsTestProcess(identifier=self.test_job1_process_id)
         self.process_store.save_process(self.process_public)
@@ -44,6 +63,7 @@ class WpsRestApiJobsTest(unittest.TestCase):
         j1.status = STATUS_SUCCEEDED
         j1.mark_finished()
         j1.progress = 100
+        j1.tags.append(VISIBILITY_PUBLIC)
         j1 = self.job_store.update_job(j1)
         self.test_job1_id = j1.id
 
@@ -54,7 +74,9 @@ class WpsRestApiJobsTest(unittest.TestCase):
                                      process=self.test_job2_process_id, service=self.test_job2_service_id,
                                      is_workflow=True, user_id=1, execute_async=False)
         j2.status = STATUS_FAILED
-        j1.progress = 99
+        j2.mark_finished()
+        j2.progress = 99
+        j2.tags.append(VISIBILITY_PUBLIC)
         j2 = self.job_store.update_job(j2)
         self.test_job2_id = j2.id
 
@@ -70,8 +92,24 @@ class WpsRestApiJobsTest(unittest.TestCase):
         j3.status = STATUS_FAILED
         j3.mark_finished()
         j3.progress = 55
+        j3.tags.append(VISIBILITY_PUBLIC)
         j3 = self.job_store.update_job(j3)
         self.test_job3_id = j3.id
+
+        # same process as Job 1, but job set to private with and editor instead
+        # (ex: job ran with private process, then process made public afterwards)
+        self.test_job4_process_id = self.test_job1_process_id
+        self.test_job4_service_id = None
+        self.test_job4_task_id = '9999-8888-7777-6666'
+        j4 = self.job_store.save_job(task_id=self.test_job4_task_id,
+                                     process=self.test_job4_process_id, service=self.test_job4_service_id,
+                                     is_workflow=False, user_id=self.user_editor1_id, execute_async=True)
+        j4.status = STATUS_FAILED
+        j4.mark_finished()
+        j4.progress = 55
+        j4.tags.append(VISIBILITY_PRIVATE)
+        j4 = self.job_store.update_job(j4)
+        self.test_job4_id = j4.id
 
     @staticmethod
     def check_job_format(job):
@@ -162,3 +200,33 @@ class WpsRestApiJobsTest(unittest.TestCase):
         resp = self.app.get(path, headers=self.json_headers)
         self.check_basic_jobs_info(resp)
         assert len(resp.json['jobs']) == 0
+
+    def test_get_service_and_process_jobs_unknown_in_path(self):
+        # path must validate each object, so error on not found process
+        path = "/service/{}/processes/{}/jobs".format('unknown-service-id', 'unknown-process-id')
+        resp = self.app.get(path, headers=self.json_headers, expect_errors=True)
+        assert resp.status_code == 404
+        assert resp.content_type == 'application/json'
+
+    def test_get_service_and_process_jobs_unknown_in_query(self):
+        # query acts as a filter, so no error on not found process
+        path = "/jobs?service={}&process={}".format('unknown-service-id', 'unknown-process-id')
+        resp = self.app.get(path, headers=self.json_headers)
+        self.check_basic_jobs_info(resp)
+        assert len(resp.json['jobs']) == 0
+
+    def test_get_jobs_with_access(self):
+        # Job 1 should have public visibility to all,
+        # Job 4 only with private access (same editor or any admin)
+
+        # test variations of paths/user request, and expected job outputs
+        path_jobs_user_req_tests = [
+            ("/jobs", UserRequest(), [self.test_job1_id]),
+            ("/processes/{}/jobs".format(self.process_public.identifier), UserRequest(), [self.test_job1_id]),
+            ("/jobs", UserRequest(), [self.test_job1_id]),
+            ("/processes/{}/jobs".format(self.process_public.identifier), UserRequest(), [self.test_job1_id]),
+        ]   # type: List[Tuple[AnyStr, UserRequest, List[AnyStr]]]
+
+        for path, expected_jobs in path_jobs_user_req_tests:
+            resp = self.app.get(path, headers=self.json_headers)
+            self.check_basic_jobs_info(resp)
