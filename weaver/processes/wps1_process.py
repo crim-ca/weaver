70e84c8a97cc278a53d98e652c0958b91a12dca8
David Byrns <david.byrns@crim.ca>
2019-03-01 15:10:29 -0500

    WP1 execute function ready

--- weaver/processes/wps1_process.py
+++ weaver/processes/wps1_process.py
@@ -1,29 +1,21 @@
+from weaver import status
+from weaver.utils import get_any_id, get_any_value, get_job_log_msg, raise_on_xml_exception
+from weaver.wps_restapi.utils import get_cookie_headers
+from weaver.execute import EXECUTE_MODE_ASYNC
+from weaver.owsexceptions import OWSNoApplicableCode
+from weaver.processes.wps_process_base import WpsProcessInterface
+from weaver.wps_restapi.processes.processes import wait_secs, _jsonify_output
+from weaver.wps_restapi.jobs.jobs import check_status
+from owslib.wps import WebProcessingService, ComplexDataInput, WPSException
+from typing import AnyStr, Callable
+from time import sleep
 import logging
 import requests
-from time import sleep
-from typing import AnyStr, Callable
-from weaver import status
-from weaver.utils import get_any_id, get_any_value, get_any_message, get_job_log_msg, pass_http_error
-from weaver.wps_restapi.swagger_definitions import (
-    process_uri,
-    process_jobs_uri,
-    process_results_uri
-)
-from weaver.processes.wps_process_base import WpsProcessInterface
-from pyramid.httpexceptions import (
-    HTTPOk,
-    HTTPNotFound,
-    HTTPInternalServerError,
-)
-
 
 LOGGER = logging.getLogger(__name__)
 
-REMOTE_JOB_PROGRESS_PROVIDER = 1
-REMOTE_JOB_PROGRESS_DEPLOY = 2
-REMOTE_JOB_PROGRESS_VISIBLE = 3
-REMOTE_JOB_PROGRESS_REQ_PREP = 5
-REMOTE_JOB_PROGRESS_EXECUTION = 9
+REMOTE_JOB_PROGRESS_REQ_PREP = 2
+REMOTE_JOB_PROGRESS_EXECUTION = 5
 REMOTE_JOB_PROGRESS_MONITORING = 10
 REMOTE_JOB_PROGRESS_FETCH_OUT = 90
 REMOTE_JOB_PROGRESS_COMPLETED = 100
@@ -38,148 +30,137 @@ class Wps1Process(WpsProcessInterface):
         # type: Callable[[AnyStr, int, AnyStr], None]
         self.update_status = update_status
 
-    def describe_process(self):
-        path = self.url + process_uri.format(process_id=self.process_id)
-        LOGGER.debug("Describe process WPS request for {0} at {1}".format(self.process_id, path))
-        response = self.make_request(method='GET',
-                                     url=path,
-                                     retry=False,
-                                     status_code_mock=HTTPOk.code)
-
-        if response.status_code == HTTPOk.code:
-            # TODO Remove patch for Geomatys ADES (Missing process return a 200 InvalidParameterValue error !)
-            if response.content.lower().find('InvalidParameterValue') >= 0:
-                return None
-            return response.json()
-        elif response.status_code == HTTPNotFound.code:
-            return None
-        # TODO Remove patch for Spacebel ADES (Missing process return a 500 error)
-        elif response.status_code == HTTPInternalServerError.code:
-            return None
-        response.raise_for_status()
-
     def execute(self, workflow_inputs, out_dir, expected_outputs):
-        self.update_status("Preparing execute request for remote ADES.",
+
+        self.update_status("Preparing execute request for remote WPS1 provider.",
                            REMOTE_JOB_PROGRESS_REQ_PREP, status.STATUS_RUNNING)
         LOGGER.debug("Execute process WPS request for {0}".format(self.process_id))
+        try:
+            try:
+                wps = WebProcessingService(url=self.provider, headers=get_cookie_headers(self.headers), verify=self.verify)
+                # noinspection PyProtectedMember
+                raise_on_xml_exception(wps._capabilities)
+            except Exception as ex:
+                raise OWSNoApplicableCode("Failed to retrieve WPS capabilities. Error: [{}].".format(str(ex)))
+            try:
+                process = wps.describeprocess(self.process_id)
+            except Exception as ex:
+                raise OWSNoApplicableCode("Failed to retrieve WPS process description. Error: [{}].".format(str(ex)))
 
-        execute_body_inputs = []
-        execute_req_id = 'id'
-        execute_req_inpt_val = 'href'
-        execute_req_out_trans_mode = 'transmissionMode'
-        for workflow_input_key, workflow_input_value in workflow_inputs.items():
-            if isinstance(workflow_input_value, list):
-                for workflow_input_value_item in workflow_input_value:
-                    execute_body_inputs.append({execute_req_id: workflow_input_key,
-                                                execute_req_inpt_val: workflow_input_value_item['location']})
-            else:
-                execute_body_inputs.append({execute_req_id: workflow_input_key,
-                                            execute_req_inpt_val: workflow_input_value['location']})
-        for exec_input in execute_body_inputs:
-            if exec_input[execute_req_inpt_val].startswith('file://'):
-                exec_input[execute_req_inpt_val] = self.host_file(exec_input[execute_req_inpt_val])
-                LOGGER.debug("Hosting intermediate input {0} : {1}".format(
-                    exec_input[execute_req_id],
-                    exec_input[execute_req_inpt_val]))
+            # prepare inputs
+            complex_inputs = []
+            for process_input in process.dataInputs:
+                if 'ComplexData' in process_input.dataType:
+                    complex_inputs.append(process_input.identifier)
 
-        execute_body_outputs = [{execute_req_id: output,
-                                 execute_req_out_trans_mode: 'reference'} for output in expected_outputs]
-        self.update_status('Executing job on remote ADES.', REMOTE_JOB_PROGRESS_EXECUTION, status.STATUS_RUNNING)
+            try:
+                wps_inputs = list()
+                for workflow_input_key, workflow_input_value in workflow_inputs.items():
+                    # TODO limited to file type right now (location key, hosting file based on scheme, etc.)!!!
+                    # in case of array inputs, must repeat (id,value)
+                    input_values = [val['location'] for val in workflow_input_value] \
+                        if isinstance(workflow_input_value, list) \
+                        else [workflow_input_value['location']]
 
-        execute_body = dict(mode='async',
-                            response='document',
-                            inputs=execute_body_inputs,
-                            outputs=execute_body_outputs)
-        request_url = self.url + process_jobs_uri.format(process_id=self.process_id)
-        response = self.make_request(method='POST',
-                                     url=request_url,
-                                     json=execute_body,
-                                     retry=True)
-        if response.status_code != 201:
-            raise Exception('Was expecting a 201 status code from the execute request : {0}'.format(request_url))
+                    # we need to host file starting with file:// scheme
+                    input_values = [self.host_file(val)
+                                    if val.startswith('file://')
+                                    else val for val in input_values]
 
-        job_status_uri = response.headers['Location']
-        job_status = self.get_job_status(job_status_uri)
-        job_status_value = status.map_status(job_status['status'])
+                    # need to use ComplexDataInput structure for complex input
+                    wps_inputs.extend([(workflow_input_key,
+                                        ComplexDataInput(input_value) if workflow_input_key in complex_inputs
+                                        else input_value) for input_value in input_values])
+            except KeyError:
+                wps_inputs = []
 
-        self.update_status('Monitoring job on remote ADES : {0}'.format(job_status_uri),
-                           REMOTE_JOB_PROGRESS_MONITORING, status.STATUS_RUNNING)
+            # prepare outputs
+            outputs = [(o.identifier, o.dataType == 'ComplexData') for o in process.processOutputs
+                       if o.identifier in expected_outputs]
 
-        while job_status_value not in status.job_status_categories[status.STATUS_CATEGORY_FINISHED]:
-            sleep(5)
-            job_status = self.get_job_status(job_status_uri)
-            job_status_value = status.map_status(job_status['status'])
+            self.update_status('Executing job on remote WPS1 provider.',
+                               REMOTE_JOB_PROGRESS_EXECUTION, status.STATUS_RUNNING)
 
-            LOGGER.debug("Monitoring job {jobID} : [{status}] {percentCompleted}  {message}".format(
-                jobID=job_status['jobID'],
-                status=job_status_value,
-                percentCompleted=job_status.get('percentCompleted', ''),
-                message=get_any_message(job_status)
-            ))
-            self.update_status(get_job_log_msg(status=job_status_value,
-                                               message=get_any_message(job_status),
-                                               progress=job_status.get('percentCompleted', 0),
-                                               duration=job_status.get('duration', None)),  # get if available
-                               self.map_progress(job_status.get('percentCompleted', 0),
-                                                 REMOTE_JOB_PROGRESS_MONITORING, REMOTE_JOB_PROGRESS_FETCH_OUT),
-                               status.STATUS_RUNNING)
+            mode = EXECUTE_MODE_ASYNC
+            execution = wps.execute(self.process_id, inputs=wps_inputs, output=outputs, mode=mode, lineage=True)
+            if not execution.process and execution.errors:
+                raise execution.errors[0]
 
-        if job_status_value != status.STATUS_SUCCEEDED:
-            LOGGER.debug("Monitoring job {jobID} : [{status}] {percentCompleted}  {message}".format(
-                jobID=job_status['jobID'],
-                status=job_status_value,
-                percentCompleted=job_status.get('percentCompleted', ''),
-                message=get_any_message(job_status)
-            ))
-            raise Exception(job_status)
+            self.update_status('Monitoring job on remote WPS1 provider : {0}'.format(self.provider),
+                               REMOTE_JOB_PROGRESS_MONITORING, status.STATUS_RUNNING)
 
-        self.update_status('Fetching job outputs from remote ADES.',
-                           REMOTE_JOB_PROGRESS_FETCH_OUT, status.STATUS_RUNNING)
-        results = self.get_job_results(job_status['jobID'])
-        for result in results:
-            if get_any_id(result) in expected_outputs:
-                # This is where cwl expect the output file to be written
-                # TODO We will probably need to handle multiple output value...
-                dst_fn = '/'.join([out_dir.rstrip('/'), expected_outputs[get_any_id(result)]])
+            max_retries = 5
+            num_retries = 0
+            run_step = 0
+            while execution.isNotComplete() or run_step == 0:
+                if num_retries >= max_retries:
+                    raise Exception("Could not read status document after {} retries. Giving up.".format(max_retries))
+                try:
+                    execution = check_status(url=execution.statusLocation, verify=self.verify,
+                                             sleep_secs=wait_secs(run_step))
 
-                # TODO Should we handle other type than File reference?
-                r = requests.get(get_any_value(result), allow_redirects=True)
-                LOGGER.debug('Fetching result output from {0} to cwl output destination : {1}'.format(
-                    get_any_value(result),
-                    dst_fn
+                    LOGGER.debug("Monitoring job {jobID} : [{status}] {percentCompleted}  {message}".format(
+                        jobID=execution.jobID,
+                        status=status.map_status(execution.getStatus()),
+                        percentCompleted=execution.percentCompleted,
+                        message=execution.statusMessage
+                    ))
+                    self.update_status(get_job_log_msg(status=status.map_status(execution.getStatus()),
+                                                       message=execution.statusMessage,
+                                                       progress=execution.percentCompleted,
+                                                       duration=None),  # get if available
+                                       self.map_progress(execution.percentCompleted,
+                                                         REMOTE_JOB_PROGRESS_MONITORING, REMOTE_JOB_PROGRESS_FETCH_OUT),
+                                       status.STATUS_RUNNING)
+                except Exception as exc:
+                    num_retries += 1
+                    LOGGER.debug('Exception raised: {}'.format(repr(exc)))
+                    sleep(1)
+                else:
+                    num_retries = 0
+                    run_step += 1
+
+            if not execution.isSucceded():
+                LOGGER.debug("Monitoring job {jobID} : [{status}] {percentCompleted}  {message}".format(
+                    jobID=execution.jobID,
+                    status=status.map_status(execution.getStatus()),
+                    percentCompleted=execution.percentCompleted,
+                    message=execution.statusMessage or "Job failed."
                 ))
-                with open(dst_fn, mode='wb') as dst_fh:
-                    dst_fh.write(r.content)
+                raise Exception(execution.statusMessage or "Job failed.")
 
-        self.update_status('Execution on remote ADES completed.',
+            self.update_status('Fetching job outputs from remote WPS1 provider.',
+                               REMOTE_JOB_PROGRESS_FETCH_OUT, status.STATUS_RUNNING)
+
+            process = wps.describeprocess(self.process_id)
+            output_datatype = {
+                getattr(processOutput, 'identifier', ''): processOutput.dataType
+                for processOutput in getattr(process, 'processOutputs', [])
+            }
+            results = [_jsonify_output(output, output_datatype[output.identifier])
+                       for output in execution.processOutputs]
+            for result in results:
+                if get_any_id(result) in expected_outputs:
+                    # This is where cwl expect the output file to be written
+                    # TODO We will probably need to handle multiple output value...
+                    dst_fn = '/'.join([out_dir.rstrip('/'), expected_outputs[get_any_id(result)]])
+
+                    # TODO Should we handle other type than File reference?
+                    r = requests.get(get_any_value(result), allow_redirects=True)
+                    LOGGER.debug('Fetching result output from {0} to cwl output destination : {1}'.format(
+                        get_any_value(result),
+                        dst_fn
+                    ))
+                    with open(dst_fn, mode='wb') as dst_fh:
+                        dst_fh.write(r.content)
+
+        except (WPSException, Exception) as exc:
+            if isinstance(exc, WPSException):
+                errors = "[{0}] {1}".format(exc.locator, exc.text)
+            else:
+                exception_class = "{}.{}".format(type(exc).__module__, type(exc).__name__)
+                errors = "{0}: {1}".format(exception_class, exc.message)
+            raise Exception(errors)
+
+        self.update_status('Execution on remote WPS1 provider completed.',
                            REMOTE_JOB_PROGRESS_COMPLETED, status.STATUS_SUCCEEDED)
-
-    def get_job_status(self, job_status_uri, retry=True):
-        response = self.make_request(method='GET',
-                                     url=job_status_uri,
-                                     retry=True,
-                                     status_code_mock=HTTPNotFound.code)
-        # Retry on 404 since job may not be fully ready
-        if retry and response.status_code == HTTPNotFound.code:
-            sleep(5)
-            return self.get_job_status(job_status_uri, retry=False)
-
-        response.raise_for_status()
-        job_status = response.json()
-
-        # TODO Remove patch for Geomatys not conforming to the status schema
-        #  - jobID is missing
-        #  - handled by 'map_status': status are upper cases and succeeded process are indicated as successful
-        job_id = job_status_uri.split('/')[-1]
-        if 'jobID' not in job_status:
-            job_status['jobID'] = job_id
-        job_status['status'] = status.map_status(job_status['status'])
-        return job_status
-
-    def get_job_results(self, job_id):
-        result_url = self.url + process_results_uri.format(process_id=self.process_id, job_id=job_id)
-        response = self.make_request(method='GET',
-                                     url=result_url,
-                                     retry=True)
-        response.raise_for_status()
-        return response.json().get('outputs', {})
