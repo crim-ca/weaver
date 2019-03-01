c2070ff02973136f46736035903b24672ab8ac83
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2019-03-01 16:56:23 -0500

    [WIP]

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ usage() {
 
     Options:
         -h   - Print this help message.
-        -i   - Installs required system packages for Birdhouse build. You *need* 'sudo' priviliges!"
+        -i   - Installs required system packages for Birdhouse build. You *need* 'sudo' privileges!"
 EOT
     exit 1
 }
@@ -14,24 +14,24 @@ EOT
 install_pkgs() {
     if [[ $EUID -eq 0 ]]; then
         echo "Enable sudo ..."
-        if [ -f /etc/debian_version ] ; then
+        if [[ -f /etc/debian_version ]] ; then
             apt-get update -y && apt-get install -y sudo
-        elif [ -f /etc/redhat-release ] ; then
+        elif [[ -f /etc/redhat-release ]] ; then
             yum update -y && yum install -y sudo
         fi
     fi
 
-    if [ -f /etc/debian_version ] ; then
+    if [[ -f /etc/debian_version ]] ; then
         echo "Install Debian/Ubuntu packages for Birdhouse build ..."
         sudo apt-get update && sudo apt-get -y install wget curl build-essential unzip
         sudo apt-get -y install vim-common # anaconda needs xxd
-    elif [ -f /etc/redhat-release ] ; then
+    elif [[ -f /etc/redhat-release ]] ; then
         echo "Install CentOS packages for Birdhouse build ..."
         sudo yum update -y && sudo yum install -y epel-release wget curl gcc-c++ make tar bzip2 unzip
         # xlibs used by cairo
         sudo yum install -y libXrender libXext libX11
         sudo yum install -y vim-common  # anaconda needs xxd
-    elif [ `uname -s` = "Darwin" ] ; then
+    elif [[ `uname -s` = "Darwin" ]] ; then
         echo "Install Homebrew packages for Birdhouse build ..."
         brew install wget curl libmagic
     fi
@@ -40,7 +40,7 @@ install_pkgs() {
 bootstrap() {
     echo "Bootstrapping ..."
 
-    if [ $# -eq 0 ] || [ $1 = '-b' ] || [ $1 = '-i' ]; then
+    if [[ $# -eq 0 ]] || [[ $1 = '-b' ]] || [[ $1 = '-i' ]]; then
         install_pkgs
     fi
 
@@ -49,16 +49,16 @@ bootstrap() {
 
 # Handling arguments
 
-if [ $# -gt 1 ]; then
+if [[ $# -gt 1 ]]; then
     echo -e "Too many arguments.\n"
     usage
 fi
 
-if [ $# -gt 0 ] && [ $1 = '-h' ]; then
+if [[ $# -gt 0 ]] && [[ $1 = '-h' ]]; then
     usage
 fi
 
-if [ $# -eq 0 ] || [ $1 = '-b' ] || [ $1 = '-i' ]; then
+if [[ $# -eq 0 ]] || [[ $1 = '-b' ]] || [[ $1 = '-i' ]]; then
     bootstrap $@
 else
     echo -e "Unknown option: $1.\n"
