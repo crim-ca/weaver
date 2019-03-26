1898c4fcea21a0cddc539d390cfdcebe1259ec57
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2019-03-26 17:15:36 -0400

    Bump version: 0.2.0-rc → 0.2.0

--- weaver/__meta__.py
+++ weaver/__meta__.py
@@ -1,29 +1,29 @@
-__name__ = 'weaver'
-__title__ = 'weaver'
-__version__ = '0.2.0-rc'
-__description__ = 'Workflow Execution Management Service (EMS).'
-__source_repository__ = 'https://www.crim.ca/stash/projects/OGC/repos/weaver'
-__docker_repository__ = 'docker-registry.crim.ca/ogc/weaver:latest'
-__license__ = 'Apache License 2.0'
+__name__ = "weaver"
+__title__ = "weaver"
+__version__ = "0.2.0"
+__description__ = "Workflow Execution Management Service (EMS)."
+__source_repository__ = "https://www.crim.ca/stash/projects/OGC/repos/weaver"
+__docker_repository__ = "docker-registry.crim.ca/ogc/weaver:latest"
+__license__ = "Apache License 2.0"
 __authors__ = [
-    'Birdhouse Developers',
-    'CRIM',
+    "Birdhouse Developers",
+    "CRIM",
 ]
 __emails__ = [
-    ''
+    ""
 ]
 __keywords__ = [
-    'buildout',
-    'pyramid',
-    'weaver',
-    'birdhouse',
-    'wps',
-    'pywps',
-    'ows',
-    'ogc',
-    'ems',
-    'ades',
-    'workflow',
-    'cwl',
-    'docker',
+    "buildout",
+    "pyramid",
+    "weaver",
+    "birdhouse",
+    "wps",
+    "pywps",
+    "ows",
+    "ogc",
+    "ems",
+    "ades",
+    "workflow",
+    "cwl",
+    "docker",
 ]
