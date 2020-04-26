429bb773090d44f0aa5d266150afa3e2f2752776
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-04-26 15:05:38 -0400

    swagger rendering updates

--- weaver/__meta__.py
+++ weaver/__meta__.py
@@ -6,6 +6,7 @@ __description__ = "Workflow Execution Management Service (EMS); Application, Dep
 # __docker_repository__ = "docker-registry.crim.ca/ogc/weaver:latest"
 __source_repository__ = "https://github.com/crim-ca/weaver"
 __docker_repository__ = "https://hub.docker.com/r/pavics/weaver"
+__documentation_url__ = "https://pavics-weaver.readthedocs.io/"
 __license_type__ = "Apache License 2.0"
 __license_short__ = "2020, CRIM"
 __authors__ = [
