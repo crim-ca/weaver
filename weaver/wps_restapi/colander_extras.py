90b362b5f8adbaf174c1c24fb5395fb7b21ffa7d
Francis Charette-Migneault <francis.charette.migneault@gmail.com>
2020-04-27 22:14:54 -0400

    tweaks to build OpenAPI-3 docs

--- weaver/wps_restapi/colander_extras.py
+++ weaver/wps_restapi/colander_extras.py
@@ -746,6 +746,7 @@ class OneOfKeywordSchema(KeywordMapper):
 
     @classmethod
     @abstractmethod
+
     def _one_of(cls):
         # type: () -> Iterable[colander._SchemaMeta]  # noqa: W0212
         """This must be overridden in the schema definition using it."""
