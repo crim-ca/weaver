696a03f21a732ad449f7ca57d28694f5665c2525
Mathieu Provencher <mathieu.provencher@crim.ca>
2020-06-17 12:06:06 -0400

    check-lint corrections

--- weaver/processes/esgf_process.py
+++ weaver/processes/esgf_process.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from os.path import join
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from weaver.status import STATUS_RUNNING, STATUS_SUCCEEDED, STATUS_FAILED
 from weaver.processes.wps1_process import Wps1Process
