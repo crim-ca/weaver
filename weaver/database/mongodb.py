bd0fc7e0588aa8f4f2b31b8d642b88d5dfe8315b
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2021-12-14 19:51:01 -0500

    pin pymongo<4 (relates to #386)

--- weaver/database/mongodb.py
+++ weaver/database/mongodb.py
@@ -66,6 +66,8 @@ class MongoDatabase(DatabaseInterface):
         self._database = get_mongodb_engine(container)
         self._settings = get_settings(container)
         self._stores = dict()
+        LOGGER.debug("Database [%s] using versions: {MongoDB: %s, pymongo: %s}",
+                     self._database.name, self._database.client.server_info()["version"], pymongo.__version__)
 
     def reset_store(self, store_type):
         store_type = self._get_store_type(store_type)
