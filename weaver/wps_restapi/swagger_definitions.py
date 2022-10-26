08591a95678f5cf8820e1a0b39a6b0be5a95f9cb
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2022-10-26 18:50:16 -0400

    add provider to groups query

--- weaver/wps_restapi/swagger_definitions.py
+++ weaver/wps_restapi/swagger_definitions.py
@@ -4714,7 +4714,7 @@ class GetJobsQueries(ExtendedMappingSchema):
     groups = ExtendedSchemaNode(
         String(), default=None, example="process,service", missing=drop,
         description="Comma-separated list of grouping fields with which to list jobs.",
-        validator=StringOneOf(["process", "service", "status"], delimiter=",", case_sensitive=True),
+        validator=StringOneOf(["process", "provider", "service", "status"], delimiter=",", case_sensitive=True),
     )
     page = ExtendedSchemaNode(Integer(allow_string=True), missing=0, default=0, validator=Range(min=0))
     limit = ExtendedSchemaNode(Integer(allow_string=True), missing=10, default=10, validator=Range(min=1, max=10000))
