9d41535b5d9f5632d0dcd873bdfc297621409e02
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2023-02-28 19:00:36 -0500

    ignore expected warnings by weaver configuration for tests

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import warnings
+
+warnings.filterwarnings("ignore", "No file specified for WPS-1 providers registration.", RuntimeWarning)
+warnings.filterwarnings("ignore", "configuration setting [weaver.cwl_processes_dir] .*", RuntimeWarning)
