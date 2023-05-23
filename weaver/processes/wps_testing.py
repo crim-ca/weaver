714f2b42e06baf2cd5fe443f0ae93a28d4eec8ef
Felix Bachand <felix.bachand@crim.ca>
2023-05-23 10:57:30 -0400

    Part 2 of adding inputs and outputs for Echo process

--- weaver/processes/wps_testing.py
+++ weaver/processes/wps_testing.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from pywps import ComplexInput
+from pywps import ComplexInput, LiteralOutput, ComplexOutput
 from pywps.app import Process
 from pywps.inout import LiteralInput
 from pywps.validator.mode import MODE
@@ -14,6 +14,118 @@ if TYPE_CHECKING:
 
     from weaver.processes.wps_package import ANY_IO_Type
 
+DATA_FORMAT_COMPLEX = {
+    "mime_type": "application/json",
+    "encoding": "",
+    "schema": {
+        "oneOf": [
+            {
+                "type": "string",
+                "contentMediaType": "application/json"
+            },
+            {
+                "type": "object",
+                "properties":
+                    {
+                        "property1": {"type": "string"},
+                        "property2": {"type": "string",
+                                      "format": "uri"},
+                        "property3": {"type": "number"},
+                        "property4": {"type": "string",
+                                      "format": "date-time"},
+                        "property5": {"type": "boolean"}
+                    },
+                "required": ["property1", "property5"]
+            }
+        ]
+    },
+    "extension": ""
+}
+
+DATA_FORMAT_GEOMETRY = {
+    "mime_type": "application/gml+xml; version=3.2",
+    "encoding": "",
+    "schema": {
+        "type": "array",
+        "items": {
+            "oneOf": [
+                {
+                    "type": "string",
+                    "contentMediaType": "application/gml+xml; version=3.2",
+                    "contentSchema": "http://schemas.opengis.net/gml/3.2.1"
+                                     "/geometryBasic2d.xsd"
+                },
+                {
+                    "type": "string",
+                    "contentMediaType": "application/json",
+                    "contentSchema": "http://schemas.opengis.net/ogcapi"
+                                     "/features/part1/1.0/openapi/schemas"
+                                     "/geometryGeoJSON.yaml"
+                }
+            ],
+            "allOf": [
+                {
+                    "format": "geojson-geometry"
+                },
+                {
+                    "$ref": "http://schemas.opengis.net/ogcapi/features/part1/1"
+                            ".0/openapi/schemas/geometryGeoJSON.yaml"
+                }
+            ]
+        }
+    }
+}
+
+DATA_FORMAT_IMAGES = {
+    "mime_type": "application/tiff; application=geotiff",
+    "encoding": "binary",
+    "schema": {
+        "oneOf": [
+            {
+                "type": "string",
+                "contentEncoding": "binary",
+                "contentMediaType": "application/tiff; application=geotiff"
+            },
+            {
+                "type": "string",
+                "contentEncoding": "binary",
+                "contentMediaType": "application/jp2"
+            }
+        ]
+    }
+}
+
+DATA_FORMAT_FEATURES_COLLECTION = {
+    "mime_type": "application/json",
+    "encoding": "",
+    "schema": {
+        "oneOf": [
+            {
+                "type": "string",
+                "contentMediaType": "application/gml+xml; version=3.2"
+            },
+            {
+                "type": "string",
+                "contentSchema": "https://schemas.opengis.net/kml/2.3"
+                                 "/ogckml23.xsd",
+                "contentMediaType": "application/vnd.google-earth.kml"
+                                    "+xml"
+            },
+            {
+                "allOf": [
+                    {
+                        "format": "geojson-feature-collection"
+                    },
+                    {
+                        "$ref": "https://geojson.org/schema"
+                                "/FeatureCollection.json"
+                    }
+                ]
+            }
+        ]
+    }
+}
+
 
 # FIXME: transform into official test EchoProcess (https://github.com/crim-ca/weaver/issues/379)
 class WpsTestProcess(Process):
@@ -23,8 +135,8 @@ class WpsTestProcess(Process):
 
     type = ProcessType.TEST  # allows to map WPS class
 
-    def __init__(self):
-        # type: () -> None
+    def __init__(self, identifier="echo-process"):
+        # type: (str) -> None
         """
         Initialize the test process with the definition of the OGC echo process.
         """
@@ -39,6 +151,7 @@ class WpsTestProcess(Process):
         metadata = [json2wps_field(meta, "metadata") for meta in metadata]
 
         super(WpsTestProcess, self).__init__(
+            identifier,
             self._handler,
             title=title,
             version=version,
@@ -71,10 +184,10 @@ class WpsTestProcess(Process):
                                      data_type="float",
                                      mode=MODE.SIMPLE)
 
-        double_input = LiteralInput("measure-input",
-                                    "Numerical Value with UOM Example",
-                                    abstract="This is an example of a NUMERIC literal with an associated unit of "
-                                             "measure.",
+        double_input = LiteralInput("double-input",
+                                    "Bounded Double Literal Input Example",
+                                    abstract="This is an example of a DOUBLE literal input that is bounded between a "
+                                             "value greater than 0 and 10.  The default value is 5.",
                                     data_type="float",
                                     mode=MODE.SIMPLE)
 
@@ -91,42 +204,108 @@ class WpsTestProcess(Process):
         complex_object_input = ComplexInput("complex-object-input",
                                             "Complex Object Input Example",
                                             abstract="This is an example of a complex object input.",
-                                            data_format={
-                                                "mime_type": "application/json",
-                                                "encoding": "",
-                                                "schema": {
-                                                    "oneOf": [
-                                                        {
-                                                            "type": "string",
-                                                            "contentMediaType": "application/json"
-                                                        },
-                                                        {
-                                                            "type": "object",
-                                                            "properties":
-                                                                {
-                                                                    "property1": {"type": "string"},
-                                                                    "property2": {"type": "string",
-                                                                                  "format": "uri"},
-                                                                    "property3": {"type": "number"},
-                                                                    "property4": {"type": "string",
-                                                                                  "format": "date-time"},
-                                                                    "property5": {"type": "boolean"}
-                                                                },
-                                                            "required": ["property1", "property5"]
-                                                        }
-                                                    ]
-                                                },
-                                                "extension": ""
-                                            },
+                                            data_format=DATA_FORMAT_COMPLEX,
+                                            supported_formats=[DATA_FORMAT_COMPLEX],
                                             mode=MODE.NONE)
 
-        return [string_input, date_input, measure_input, double_input, array_input, complex_object_input]
+        geometry_input = ComplexInput("geometry-input",
+                                      "Geometry input",
+                                      abstract="This is an example of a geometry input.  In this case the geometry "
+                                               "can be expressed as a GML of GeoJSON geometry.",
+                                      data_format=DATA_FORMAT_GEOMETRY,
+                                      supported_formats=[DATA_FORMAT_GEOMETRY],
+                                      min_occurs=2,
+                                      max_occurs=5,
+                                      mode=MODE.NONE)
+
+        images_input = ComplexInput("images-input",
+                                    "Inline Images Value Input",
+                                    abstract="This is an example of an image input.  In this case, the input is an "
+                                             "array of up to 150 images that might, for example, be a set of tiles.  "
+                                             "The oneOf[] conditional is used to indicate the acceptable image "
+                                             "content types; GeoTIFF and JPEG 2000 in this case.  Each input image in "
+                                             "the input array can be included inline in the execute request as a "
+                                             "base64-encoded string or referenced using the link.yaml schema.  The "
+                                             "use of a base64-encoded string is implied by the specification and does "
+                                             "not need to be specified in the definition of the input.",
+                                    data_format=DATA_FORMAT_IMAGES,
+                                    supported_formats=[DATA_FORMAT_IMAGES],
+                                    min_occurs=1,
+                                    max_occurs=150,
+                                    mode=MODE.NONE)
+
+        feature_collection_input = ComplexInput("feature-collection-input",
+                                                title="Feature Collection Input Example",
+                                                abstract="This is an example of an input that is a feature collection "
+                                                         "that can be encoded in one of three ways. As a GeoJSON "
+                                                         "feature collection, as a GML feature collection retrieved "
+                                                         "from a WFS or as a KML document.",
+                                                data_format=DATA_FORMAT_FEATURES_COLLECTION,
+                                                supported_formats=[DATA_FORMAT_FEATURES_COLLECTION])
+
+        return [string_input, date_input, measure_input, double_input,
+                array_input, complex_object_input, geometry_input,
+                images_input, feature_collection_input]
 
     @staticmethod
     def __get_outputs():
-        return []
+        string_output = LiteralOutput("string-output",
+                                      title="String Literal Output Example",
+                                      data_type="string",
+                                      mode=MODE.SIMPLE)
+
+        date_output = LiteralOutput("date-output",
+                                    title="Date Literal Output Example",
+                                    data_type="dateTime",
+                                    mode=MODE.SIMPLE)
+
+        measure_output = LiteralOutput("measure-output",
+                                       title="Numerical Output with UOM Example",
+                                       data_type="float",
+                                       mode=MODE.SIMPLE)
+
+        double_output = LiteralOutput("double-output",
+                                      title="Bounded Double Literal Ouput Example",
+                                      data_type="float",
+                                      mode=MODE.SIMPLE)
+
+        array_output = LiteralOutput("array-output",
+                                     title="Array Output Example",
+                                     data_type="integer",
+                                     mode=MODE.SIMPLE)
+
+        complex_object_output = ComplexOutput("complex-object-output",
+                                              title="Complex Object Output Example",
+                                              data_format=DATA_FORMAT_COMPLEX,
+                                              mode=MODE.NONE)
+
+        geometry_output = ComplexOutput("geometry-output",
+                                        title="Geometry output",
+                                        data_format=DATA_FORMAT_GEOMETRY,
+                                        mode=MODE.NONE)
+
+        images_output = ComplexOutput("images-output",
+                                      title="Inline Images Value Output",
+                                      data_format=DATA_FORMAT_IMAGES,
+                                      mode=MODE.NONE)
+
+        feature_collection_output = ComplexOutput("feature-collection-input",
+                                                  title="Feature Collection Output Example",
+                                                  data_format=DATA_FORMAT_FEATURES_COLLECTION)
+
+        return [string_output, date_output, measure_output, double_output,
+                array_output, complex_object_output, geometry_output,
+                images_output, feature_collection_output]
 
     def _handler(self, request, response):
-        response.update_status(f"WPS Test Output from process {self.identifier}...", 0)
-        response.outputs["test_output"].data = request.inputs["test_input"][0].data
+        response.update_status(f"Echo process Output from process {self.identifier}...", 0)
+        response.outputs["string-output"].data = request.inputs["string-input"].data
+        response.outputs["date-output"].data = request.inputs["date-input"].data
+        response.outputs["measure-output"].data = request.inputs["measure-input"].data
+        response.outputs["double-output"].data = request.inputs["double-input"].data
+        response.outputs["array-output"].data = request.inputs["array-input"].data
+        response.outputs["complex-object-output"].data = request.inputs["complex-object-input"].data
+        response.outputs["geometry-output"].data = request.inputs["geometry-input"].data
+        response.outputs["images-output"].data = request.inputs["images-input"].data
+        response.outputs["feature-collection-output"].data = request.inputs["feature-collection-input"].data
         return response
