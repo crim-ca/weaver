7591617dd3cc04a0b61f5ada7022dfa14ecf5f6e
Nazim Azeli <nazim.azeli@crim.ca>
2024-11-06 12:29:50 -0500

    Fixing pep8

--- tests/functional/test_cli.py
+++ tests/functional/test_cli.py
@@ -446,7 +446,8 @@ class TestWeaverClient(TestWeaverClientBase):
         for out_fmt in output_formats:
             out_fmt.pop("$schema", None)
             out_fmt.pop("$id", None)
-        assert output_formats == [{"default": True, "mediaType": ContentType.TEXT_PLAIN}, {'mediaType': 'application/pdf'}]
+        assert output_formats == [{"default": True, "mediaType": ContentType.TEXT_PLAIN},
+                                  {'mediaType': 'application/pdf'}]
         assert "undefined" not in result.message, "CLI should not have confused process description as response detail."
         assert result.body["description"] == (
             "Dummy process that simply echo's back the input message for testing purposes."
