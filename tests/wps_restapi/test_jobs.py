e03434328571d5882eacf9a55cbd1629736c6eb1
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2025-05-02 10:27:56 -0400

    fix imports linting

--- tests/wps_restapi/test_jobs.py
+++ tests/wps_restapi/test_jobs.py
@@ -58,7 +58,7 @@ if TYPE_CHECKING:
     from typing import Any, Iterable, List, Optional, Tuple, Union
 
     from weaver.status import AnyStatusType
-    from weaver.typedefs import AnyContentType, AnyLogLevel, HeadersType, KVP, JobResults, JSON, Number, Statistics
+    from weaver.typedefs import AnyContentType, AnyLogLevel, HeadersType, JobResults, JSON, KVP, Number, Statistics
     from weaver.visibility import AnyVisibility
 
 
