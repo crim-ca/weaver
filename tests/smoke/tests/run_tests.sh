c6db90894e2578ef7acb509e6d42ab210841e8aa
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2026-06-06 01:12:41 -0400

    adjust docker smoke-test pytest unavailable + add smoke-tests for unavailable pip/apt tools

--- tests/smoke/tests/run_tests.sh
+++ tests/smoke/tests/run_tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-pip install pytest
-pytest /tests -vvv -p no:cacheprovider
+# WARNING:
+#   can only use builtin unittest module to run tests within docker smoke-tests because built
+#   images remove installers limiting the runtime addition of pytest and other test runners
+python -m unittest discover -s /tests -p 'test_*.py' -v
